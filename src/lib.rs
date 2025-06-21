c6eb234a081871c8ae151af2e6553eb4ce99d272
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2025-06-21 18:40:14 +0530

    bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
-use solana_program::{pubkey::Pubkey,program::invoke_signed,instruction::{AccountMeta, Instruction},account_info::{AccountInfo,next_account_info},entrypoint,entrypoint::{ProgramResult},system_instruction::create_account};
+use solana_program::{pubkey::Pubkey,program::invoke_signed,account_info::{AccountInfo,next_account_info},entrypoint,entrypoint::{ProgramResult},system_instruction::create_account};
 
-!entrypoint(process_instruction)
+entrypoint!(process_instruction);
 
 fn process_instruction(
-    public_key:&Pubkey,//program id
+    program_id:&Pubkey,//program id
     accounts:&[AccountInfo],
     instruction_data:&[u8]
 ) -> ProgramResult {
 
     let iter  = &mut accounts.iter();
-    let pda = next_account_info(iter)?;
     let user_acc = next_account_info(iter)?;
+    let pda = next_account_info(iter)?;
     let system_program = next_account_info(iter)?;
 
-    let seeds = &[user_acc.key.as_ref(),"user-seeds"] //converting to bytes
+    let (pda_public_key,bump) = Pubkey::find_program_address(&[b"client1",user_acc.key.as_ref()],program_id);
+    let seeds = &[b"client1",user_acc.key.as_ref(),&[bump]]; //converting to bytes
 
-   let (pda_public_key,bump) = Pubkey::find_program_address(seeds,program_id);
-    let ix = create_account(user_acc.key,pda.key,1000000000,8,public_key);
+    let ix = create_account(&user_acc.key,&pda.key,1000000000,8,program_id);
 
-    invoke_signed(ix,[accounts[1],accounts[0],accounts[2]],&[&[seeds,&[bump]]]);
+    invoke_signed(&ix,accounts,&[seeds]);
 
+    Ok(())
 }
